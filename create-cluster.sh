aedc051f1cc44f4b210b26aedfa85d75492da3d2
Richard Conway <richard@rconway.co.uk>
2021-09-27 21:55:50 +0100

    Reorganise install scripts

--- create-cluster.sh
+++ create-cluster.sh
@@ -11,30 +11,11 @@ onExit() {
 }
 trap onExit EXIT
 
-# Default kube config
-export KUBECONFIG=
-
 # Cluster
+echo -e "\nCreate cluster with k3d..."
 k3d cluster create "${CLUSTER_NAME}" \
   -p "80:80@loadbalancer" \
   -p "443:443@loadbalancer" \
   --k3s-server-arg --disable=traefik \
   --agents 1 \
-  --kubeconfig-update-default=false \
   --wait
-  # --agents 1 --agents-memory 8G \
-
-# Config
-k3d kubeconfig merge "${CLUSTER_NAME}" -d -s
-
-# Nginx Ingress Controller
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-helm install ingress-nginx ingress-nginx/ingress-nginx
-
-# Cert Manager
-helm repo add jetstack https://charts.jetstack.io
-helm install cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --create-namespace \
-  --set installCRDs=true
--- eoepca.sh
+++ eoepca.sh
@@ -4,24 +4,31 @@ ORIG_DIR="$(pwd)"
 cd "$(dirname "$0")"
 BIN_DIR="$(pwd)"
 
-CLUSTER_NAME="${1:-mykube}"
+CLUSTER_NAME="${1:-eoepca}"
 
 onExit() {
   cd "${ORIG_DIR}"
 }
 trap onExit EXIT
 
-echo "Install k3d..."
+echo -e "\nInstall k3d..."
 ./install-k3d.sh
 
-echo "Create the cluster..."
-./create-cluster.sh eoepca
+echo -e "\nCreate the cluster..."
+./create-cluster.sh "${CLUSTER_NAME}"
+export KUBECONFIG=$(k3d kubeconfig write "${CLUSTER_NAME}")
 
-echo "Create letsencrypt cluster issuer..."
+echo -e "\nCluster post-installation steps..."
+./cluster-post-install.sh
+
+echo -e "\nCreate letsencrypt cluster issuer..."
 kubectl apply -f cert-manager
 
-echo "Login service pre-requisites..."
+echo -e "\neoepca helm repo..."
+helm repo add eoepca https://eoepca.github.io/helm-charts/
+
+echo -e "\nLogin service pre-requisites..."
 kubectl apply -f login-service/yaml
 
-echo "Deploy login-service..."
+echo -e "\nDeploy login-service..."
 ./login-service/login-service.sh upgrade -i
