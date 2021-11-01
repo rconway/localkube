caf0eb559607d137dc3083ed8a4ebd514cf707a5
Richard Conway <richard@rconway.co.uk>
2021-11-01 09:48:44 +0000

    Pre-create minio buckets using helm values

--- cluster/minio/minio.sh
+++ cluster/minio/minio.sh
@@ -14,8 +14,3 @@ echo -e "\nDeploy minio..."
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade -i -n minio --create-namespace minio -f minio-values.yaml bitnami/minio --wait
-
-# Create an 'eoepca' bucket
-echo -e "\nCreating 'eoepca' bucket..."
-sleep 5
-s3cmd -c ./s3cfg mb s3://eoepca
